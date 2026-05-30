906c72403ed3dcadeb15c0fbc916759023aab7b8
ksindicue-ux <ksindicue@unal.edu.co>
2026-05-29 20:40:12 -0500

    Se agregaron la lectura de archivos y como se guardan los datos en las matrices

--- xd.cpp
+++ xd.cpp
@@ -23,11 +23,46 @@ int main() {
     ifstream datos2025("input/ingresos-2025.csv");
     ofstream totales("output/totales.txt");
 
-    cout << "Creacion de variables y matrices" << endl;
+    string linea, valor;
+    int fila, columna;
+
+    fila = 0;
+    while (getline(datos2023, linea) && fila < 5) {
+        stringstream ss(linea); 
+        columna = 0;
+        while (getline(ss, valor, ',') && columna < 12) {
+             m2023[fila][columna] = stoi(valor); 
+             columna++;
+             }
+        fila++;
+    }
+    
+    fila = 0;
+    while (getline(datos2024, linea) && fila < 5) {
+        stringstream ss(linea);
+         columna = 0;
+        while (getline(ss, valor, ',') && columna < 12) {
+             m2024[fila][columna] = stoi(valor); 
+             columna++; 
+            }
+        fila++;
+    }
+
+    fila = 0;
+    while (getline(datos2025, linea) && fila < 5) {
+        stringstream ss(linea); 
+        columna = 0;
+        while (getline(ss, valor, ',') && columna < 12) { 
+            m2025[fila][columna] = stoi(valor); 
+            columna++;
+         }
+        fila++;
+    }
+
     datos2023.close();
     datos2024.close();
     datos2025.close();
     totales.close();
-
+    cout << "Codigo para la lectura de los archivos y se guardan en las matrices" << endl;
     return 0;
 }
