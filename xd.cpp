f223a0a74a0e0e7ba042697ea7523349322a12fd
ksindicue-ux <ksindicue@unal.edu.co>
2026-05-29 20:52:02 -0500

    Se agrego las operaciones de las matrices para la suma

--- xd.cpp
+++ xd.cpp
@@ -25,44 +25,52 @@ int main() {
 
     string linea, valor;
     int fila, columna;
-
+    
     fila = 0;
     while (getline(datos2023, linea) && fila < 5) {
         stringstream ss(linea); 
         columna = 0;
         while (getline(ss, valor, ',') && columna < 12) {
              m2023[fila][columna] = stoi(valor); 
-             columna++;
-             }
+             columna++; 
+            }
         fila++;
     }
     
     fila = 0;
     while (getline(datos2024, linea) && fila < 5) {
-        stringstream ss(linea);
-         columna = 0;
-        while (getline(ss, valor, ',') && columna < 12) {
-             m2024[fila][columna] = stoi(valor); 
-             columna++; 
+        stringstream ss(linea); columna = 0;
+        while (getline(ss, valor, ',') && columna < 12) { 
+            m2024[fila][columna] = stoi(valor); 
+            columna++; 
             }
         fila++;
     }
 
     fila = 0;
     while (getline(datos2025, linea) && fila < 5) {
-        stringstream ss(linea); 
-        columna = 0;
+        stringstream ss(linea); columna = 0;
         while (getline(ss, valor, ',') && columna < 12) { 
             m2025[fila][columna] = stoi(valor); 
-            columna++;
-         }
+            columna++; 
+            }
         fila++;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 12; j++) {
+            total2023[i] += m2023[i][j];
+            total2024[i] += m2024[i][j];
+            total2025[i] += m2025[i][j];
+        }
+        totalTrienal[i] = total2023[i] + total2024[i] + total2025[i];
     }
 
     datos2023.close();
     datos2024.close();
     datos2025.close();
     totales.close();
-    cout << "Codigo para la lectura de los archivos y se guardan en las matrices" << endl;
+
+    cout << "Se creo el code para las operaciones" << endl;
     return 0;
 }
